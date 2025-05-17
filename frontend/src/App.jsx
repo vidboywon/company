8b18e7c63c3d53ad646e9a5e8dff914d45203185
vidboywon <kimjw4512@gmail.com>
2025-05-17 14:57:36 +0900

    footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,14 @@
 import "./App.css";
 import Navbar  from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  // const foodItems = ['pizza', 'burger', 'sushi', 'pasta', 'salad'];
-  
-  // const FoodItem = ({ foodName }) => <div>{foodName}</div>;
   return (
     <BrowserRouter>
       <Navbar />
-      {/* <div>
-        <div className="pt-16">
-          {foodItems.map((food, index) => (
-            <FoodItem key={index} foodName={food} />
-          ))}
-        </div>
-      </div> */}
+      <div className="p-10"></div>
+      <Footer />
     </BrowserRouter>
   );
 }
